1a79f6e919b3cf15ff0fe108959bf716bdf4540d
Pål-Christian By <Pal.christian.by@gmail.com>
2019-12-11 08:42:20 +0100

    app

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className='card'>
+            <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+
+export default Card;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div>
+      <input 
+        className='search-field' 
+        type='search' 
+        placeholder='search robots'
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
+
+export default SearchBox;
