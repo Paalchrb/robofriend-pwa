68ccea26a89f51d64f3bfb043f4677d38b61982c
Pål-Christian By <Pal.christian.by@gmail.com>
2020-04-03 11:26:46 +0200

    Updates

---
+++ precache-manifest.49253240d82feb3854f9384f656ad95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66bd3f5050f817f175761f042f9b9cb",
+    "url": "/robofriend-pwa/index.html"
+  },
+  {
+    "revision": "51c1dce2e72efd2ed36e",
+    "url": "/robofriend-pwa/static/css/main.a0820007.chunk.css"
+  },
+  {
+    "revision": "62ea13f2a543db353bd0",
+    "url": "/robofriend-pwa/static/js/2.b2488d1e.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/robofriend-pwa/static/js/2.b2488d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c1dce2e72efd2ed36e",
+    "url": "/robofriend-pwa/static/js/main.daeea6cd.chunk.js"
+  },
+  {
+    "revision": "f50cce4b404d9accccfa",
+    "url": "/robofriend-pwa/static/js/runtime-main.85163cef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
